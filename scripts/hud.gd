7cdd833818a3dec5ebcadccf75cb719fab54d2b6
Craig <cdevelopment010@gmail.com>
2025-08-27 17:20:13 +0100

    Feat: Restart btn on game over

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -9,5 +9,14 @@ func show_message(text):
 	$Message.show()
 	
 func show_game_over():
+	$Button.show()
 	show_message("Game Over")
 	
+
+
+func _on_button_pressed() -> void:
+	update_score(0)
+	$Message.text = ""
+	$Message.hide()
+	$Button.hide()
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
